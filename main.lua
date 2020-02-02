c86727c284c329758729e0e985a748370559c383
groverburger <groverburger696@gmail.com>
2020-02-01 21:55:52 -0800

    fix?

--- main.lua
+++ main.lua
@@ -37,6 +37,8 @@ function love.load()
     DeathGear2 = NewAnimatedSprite("assets/deathGear2.png")
     DeathHead = NewAnimatedSprite("assets/deathHead.png")
 
+    ScreenShake = 0
+
     MikeSprite = NewAnimatedSprite("assets/mike.png")
     LaserSprite = NewAnimatedSprite("assets/laser.png")
 
@@ -144,11 +146,15 @@ function love.load()
   	love.graphics.setCanvas()
 end
 
+function ShakeScreen()
+	ScreenShake = 0.185
+end
+
 function love.update(dt)
+	ScreenShake = ScreenShake - dt
     Scene:update(dt)
     UpdateButtons()
 end
-
 
 function love.draw()
     love.graphics.setCanvas(Canvas)
@@ -156,7 +162,13 @@ function love.draw()
     Scene:draw()
     love.graphics.setCanvas()
     local scale = math.min(love.graphics.getWidth()/Width,love.graphics.getHeight()/Height)
-    love.graphics.draw(Canvas, love.graphics.getWidth()/2, love.graphics.getHeight()/2, 0, scale,scale, Width/2,Height/2)
+    local sx,sy = 0,0
+
+    if ScreenShake > 0 then
+    	sx = love.math.random()*Choose{1,-1}*scale*8
+    	sy = love.math.random()*Choose{1,-1}*scale*8
+    end
+    love.graphics.draw(Canvas, love.graphics.getWidth()/2 + sx, love.graphics.getHeight()/2 + sy, 0, scale,scale, Width/2,Height/2)
 end
 
 
@@ -298,7 +310,7 @@ function CopyTable(table)
 end
 
 function Choose(arr)
-    return arr[Floor(love.math.random()*(#arr))+1]
+    return arr[math.floor(love.math.random()*(#arr))+1]
 end
 function Rand(min,max, interval)
     local interval = interval or 1
